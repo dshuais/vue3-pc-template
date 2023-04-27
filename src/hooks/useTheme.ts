78fa3ed1ac7941c068edbceb2b6feef09b2de4fc
dushuais <1137896420@qq.com>
2023-04-27 17:42:22 +0800

    feat:新增useTheme

---
+++ src/hooks/useTheme.ts
@@ -0,0 +1,14 @@
+/*
+ * @Author: dushuai
+ * @Date: 2023-04-27 17:40:56
+ * @LastEditors: dushuai
+ * @LastEditTime: 2023-04-27 17:41:20
+ * @description: 切换主题useTheme
+ */
+
+/**
+ * @description 切换主题
+ */
+export const useTheme = () => {
+
+}
