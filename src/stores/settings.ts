eca49a1930498df656a8bc9aeeaf86e44120a62e
dshuais <1137896420@qq.com>
2023-11-20 11:28:22 +0800

    fix：修改windcss配置theme

---
+++ src/stores/settings.ts
@@ -0,0 +1,7 @@
+/*
+ * @Author: dushuai
+ * @Date: 2023-11-01 16:41:57
+ * @LastEditors: dushuai
+ * @LastEditTime: 2023-11-20 11:27:03
+ * @description: 公共配置
+ */
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,10 @@
+/*
+ * @Author: dushuai
+ * @Date: 2023-11-01 15:46:21
+ * @LastEditors: dushuai
+ * @LastEditTime: 2023-11-01 16:33:07
+ * @description: tailwindcss config
+ */
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
@@ -8,5 +15,6 @@ module.exports = {
     extend: {},
   },
   plugins: [],
+  darkMode: 'class'
 }
 
